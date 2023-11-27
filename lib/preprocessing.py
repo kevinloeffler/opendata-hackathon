fdefe7f914b9969bf70248693d89841cc60be411
kevinloeffler <hi@kevinloeffler.ch>
2023-11-27 22:48:54 +0100

    replace reformat method with sequence_data add merge_days to read_data method add get_sensor and get_training_data

--- lib/preprocessing.py
+++ lib/preprocessing.py
@@ -1,8 +1,12 @@
+import math
+
+from numpy import array
 import pandas as pd
 
 MAX_SENSOR_INPUT = 1600
 
-def read_data(from_path: str, use_coordinates: bool = False, ):
+
+def read_data(from_path: str, use_coordinates: bool = False):
     columns = [
         'Gemessen am',
         'Tags',
@@ -12,33 +16,54 @@ def read_data(from_path: str, use_coordinates: bool = False, ):
 
     if use_coordinates:
         columns.append('geo_point_2d')
+        # TODO: needs to be implemented in later functions (e.g. _merge_days) if really needed
 
-    df = pd.read_csv(from_path, delimiter=';', usecols=columns)
-    df = df.rename(columns={'Gemessen am': 'date', 'Tags': 'type', 'Füllstandsdistanz': 'level', 'Sensorname': 'sensor_id'})
+    raw_data = pd.read_csv(from_path, delimiter=';', usecols=columns)
+    raw_data = raw_data.rename(columns={'Gemessen am': 'date', 'Tags': 'type', 'Füllstandsdistanz': 'level', 'Sensorname': 'sensor_id'})
 
-    # ids = set(df['sensor_id'])
+    filtered_data = raw_data[raw_data['level'].notna()]  # filter all rows with invalid sensor data
+    days_merged = _merge_days(filtered_data)
+    days_merged['level'] = days_merged['level'].apply(lambda level: normalize_data(level))  # normalise data
+    return days_merged
 
-    # for idx in range(1, len(df)):
-        # max_value_per_sensor[df.loc[idx, 'sensor_id']] = max(max_value_per_sensor[df.loc[idx, 'sensor_id']], df.loc[idx, 'level'])
 
-    df['level'] = df['level'].apply(lambda level: normalize_data(level))
+def _merge_days(dataframe: pd.DataFrame):
+    dataframe['date'] = pd.to_datetime(dataframe['date'], utc=True).dt.date
+    return dataframe.groupby(['sensor_id', 'date']).agg({'level': 'mean', 'type': 'last'}).reset_index()
 
-    return df
+
+def get_sensor_values(dataframe: pd.DataFrame, sensor_name: str) -> list[float]:
+    return dataframe[dataframe['sensor_id'] == sensor_name]['level'].tolist()
+
+
+def get_training_data(dataframe: pd.DataFrame) -> list[float]:
+    return dataframe['level'].tolist()
 
 
 def normalize_data(level: float):
     level = min(level, MAX_SENSOR_INPUT)
+    if math.isnan(1 - (level / MAX_SENSOR_INPUT)):
+        print(level)
     return 1 - (level / MAX_SENSOR_INPUT)
 
-def reformat(data: pd.DataFrame, step: int) -> tuple[pd.DataFrame, pd.DataFrame]:
-    data_slice = data['level']
-    x_data = []
-    y_data = []
-    for i in range(step, len(data)-step):
-        x_data.append(data_slice[i-step:i])
-        y_data.append(data_slice[i:])
-                      
-    return x_data, y_data
+
+def sequence_data(data: list[float], step_size: int, sensor_noise: float = 0.1):
+    x, y = [], []
+    for i in range(len(data) - step_size):
+        sub_array = data[i: i + step_size]
+        if not check_if_array_is_ascending(sub_array) or data[i + step_size] < data[i + step_size - 1]:  # + sensor_noise:
+            continue
+        x.append(data[i: i + step_size])
+        y.append(data[i + step_size])
+    return array(x), array(y)
+
+
+def check_if_array_is_ascending(array: list[float]) -> bool:
+    for i in range(len(array) - 1):
+        if array[i] > array[i + 1]:
+            return False
+    return True
+
 
 def split_data(data: pd.DataFrame, ratio: float) -> tuple[pd.DataFrame, pd.DataFrame]:
     total_len = len(data)
