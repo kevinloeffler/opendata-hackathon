09d0d104708c8dcc486f41229f2c7b80a929db45
patrick.wissiak <patrick.wissiak@ost.ch>
2023-11-28 14:32:00 +0100

    Create optimal path and generate map

--- lib/path-finder.py
+++ lib/path-finder.py
@@ -10,6 +10,8 @@ from itertools import combinations
 from datetime import datetime
 import numpy as np
 import os
+from googlemaps.maps import StaticMapPath
+from googlemaps.maps import StaticMapMarker
 
 with open('.env', 'r') as fh:
     vars_dict = dict(
@@ -22,7 +24,7 @@ sensor_data = read_data('data/fill-level.csv', use_coordinates=True)
 gmaps = googlemaps.Client(key=vars_dict["MAPS_KEY"])
 
 dist_file = 'data/distances.npy'
-n = 10 # Take a look at the n highest sensor levels of the next day
+n = 50 # Take a look at the n highest sensor levels of the next day
 time_per_working_day = 8 * 60 * 60 # 8 hours in seconds
 time_per_emptying = 30 * 60 # 30 minutes in seconds
 day = datetime(2023, 12, 4, 10, 00) # Date of prediction
@@ -52,16 +54,17 @@ def get_distances(n):
 
         # TODO: check if this makes sense:
         # Set weights of edges to inverse of level given by the sensor
-        distances[node_from+1][node_to+1] = distance_in_seconds / sensor_to["level"]
-        distances[node_to+1][node_from+1] = distance_in_seconds / sensor_from["level"]
+        distances[node_from][node_to] = distance_in_seconds / sensor_to["level"]
+        distances[node_to][node_from] = distance_in_seconds / sensor_from["level"]
 
+    # Get distances to sensor_0 which
     for node_to in range(n):
         sensor_to = sensor_data.iloc[node_to]
         origins = [
             station_0
         ]
         destinations = [
-            sensor_to["geo_point_2d"].split(",")
+            sensor_to["geo_point_2d"].split(", ")
         ]
 
         matrix = gmaps.distance_matrix(
@@ -76,8 +79,8 @@ def get_distances(n):
 
         # TODO: check if this makes sense:
         # Set weights of edges to inverse of level given by the sensor
-        distances[0][node_to+1] = distance_in_seconds / sensor_to["level"]
-        distances[node_to+1][0] = distance_in_seconds
+        distances[-1][node_to] = distance_in_seconds / sensor_to["level"]
+        distances[node_to][-1] = distance_in_seconds
 
     np.save(dist_file, distances)
     return distances
@@ -95,3 +98,84 @@ print(distances)
 
 # TODO: always take nearest node and add it to the graph while needed_time < time_per_working_day
 # TODO: add daily weights if nodes have not been processed today
+
+needed_time = 0
+current_stop_idx = -1 #station_0 index
+visited_stops = []
+
+# distances[current_stop_idx, 0] is the time needed from the station to station_0
+while(needed_time < (time_per_working_day - distances[current_stop_idx, -1] - time_per_emptying)):
+    visited_stops.append(current_stop_idx)
+
+    if len(visited_stops) == n+1:
+        # all stops visited
+        break
+    min_cost = np.min(np.delete(distances[0,:], visited_stops, axis=0)) # Min cost of unvisited stops
+    for idx in np.argwhere(distances[0,:] == min_cost).ravel():
+        if idx not in visited_stops:
+            next_stop_idx = idx
+    
+    needed_time += min_cost + time_per_emptying
+    current_stop_idx = next_stop_idx
+
+visited_stops.append(-1) # End at station_0
+needed_time += distances[current_stop_idx, 0] # Add time to go to station_0
+    
+print("Needed time:")
+print(needed_time)
+print("Trajectory:")
+for stop in visited_stops:
+    if stop == 0:
+        print(stop)
+    else:
+        print(f"{stop} -> {levels[stop-1]}")
+
+unvisited_stops = np.setdiff1d(range(n), visited_stops)
+print("Unvisited:")
+print(unvisited_stops)
+print("Filling levels:")
+print(levels)
+
+points = []
+markers = []
+for stop in visited_stops:
+    if stop != -1:
+        sensor = sensor_data.iloc[stop]
+        location = sensor["geo_point_2d"].split(", ")
+        markers.append(StaticMapMarker(
+            locations=[location],
+            size="tiny",
+            color="red",
+            #label=chr(ord('a') + -32 + stop),
+        ))
+    else:
+        location = station_0
+    points.append(location)
+markers.append(StaticMapMarker(
+    locations=[station_0],
+    size="tiny",
+    color="blue",
+))
+
+st_gallen = (47.4245, 9.3767)
+path = StaticMapPath(
+    points=points,
+    weight=1,
+    color="blue",
+)
+response = gmaps.static_map(
+    size=(400, 400),
+    zoom=12,
+    center=st_gallen,
+    maptype="satellite",
+    format="png",
+    scale=2,
+    path=path,
+    markers=markers
+)
+print()
+f = open("map-output.png", 'wb')
+for chunk in response:
+    if chunk:
+        f.write(chunk)
+f.close()
