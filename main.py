9a10a67b62f92c6e600aa9f4158e1b3f6ff9729d
Wissiak <pwissiak@hispeed.ch>
2023-12-02 22:14:07 +0100

    Load new model data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from numpy import array
+import pandas as pd
 
 from lib.models.vanilla_lstm import VanillaLSTM
 from lib.preprocessing import read_data, sequence_data, get_sensor_values, get_training_data, split_data
@@ -6,8 +7,11 @@ from lib.preprocessing import read_data, sequence_data, get_sensor_values, get_t
 
 STEP_SIZE = 5
 
-
-raw_data = read_data('data/fill-level.csv')
+columns = [
+    'sensor_id','date','geo_point_2d','level','type'
+]
+raw_data = pd.read_csv("data/days_merged.csv", delimiter=',', usecols=columns)
+raw_data.sort_values(["sensor_id", "date"], inplace=True)
 # test_sensor = get_sensor_values(data, '107075 | 2B2A')
 raw_train, raw_test = split_data(data=raw_data, ratio=0.9)
 train = get_training_data(raw_train)
@@ -19,12 +23,12 @@ test_x, test_y = sequence_data(test, STEP_SIZE)
 #    print(x, '->', train_y[index])
 
 # EXAMPLE: Create model
-'''
+
 vanilla_lstm_model = VanillaLSTM(step_size=STEP_SIZE)
-vanilla_lstm_model.train(train_x, train_y, 40, 'trained_models/vanilla-lstm')
+vanilla_lstm_model.train(train_x, train_y, 10, 'trained_models/vanilla-lstm')
 accuracy = vanilla_lstm_model.test(test_x, test_y)
 print('accuracy:', round(100 * accuracy, 3), '%')
-'''
+
 
 # EXAMPLE: Load model from disk
 model = VanillaLSTM(step_size=STEP_SIZE, load_from='trained_models/vanilla-lstm-1')
