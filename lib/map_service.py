fix bug in path calculation

lib/map_service.py
@@ -84,7 +84,7 @@ class MapService:
        
         for node_to in range(self.n_sensors):
             sensor_to = self.sensor_data.iloc[node_to]
-            cost_matrix[-1][node_to] = dist_matrix[-1][node_to] / sensor_to["level"]
+            cost_matrix[-1][node_to] = dist_matrix[-1][node_to] / sensor_to["level"] if sensor_to["level"] > self.no_empty_if_below else np.inf
             cost_matrix[node_to][-1] = dist_matrix[node_to][-1]
 
         return cost_matrix

lib/path_api.py
@@ -56,9 +56,10 @@ def get_next_n_days(n_days: int, no_empty_if_below: float):
         print(needed_capacity)
         print(visited_stations_by_id)
         most_left_point = np.argmin([float(x["lat"]) for x in visited_stations[1:-1]])
-        tour, locations = path_finder.refine_path(most_left_point, visited_stops[1:-1], visited_stations[1:-1])
+        _, locations = path_finder.refine_path(most_left_point, visited_stops[1:-1], visited_stations[1:-1])
         locations = [station_0] + locations + [station_0]
-        print(tour)
+        print("Refined path:")
+        print([x["sensor_id"] for x in locations[1:-1]])
 
         all_needed_time.append(needed_time)
         all_needed_capacity.append(needed_capacity)